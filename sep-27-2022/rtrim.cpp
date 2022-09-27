e6f4ace62095d8a676c6694c23926dd441ec1ce8
Hayk Danghyan <haykhermine07@gmail.com>
2022-09-27 20:54:02 +0400

    function that removes the spaces from the end of the string

--- sep-27-2022/rtrim.cpp
+++ sep-27-2022/rtrim.cpp
@@ -8,19 +8,21 @@ bool is_char(const char ch) {
 }
 
 void rtrim(std::string& str) {
+	int count = 0;
 	for (int i = str.length() - 1; i >= 0; --i) {
 		if (str[i] == ' ') {
-			str.erase(str.begin() + i);
-		}	
-		if (is_char(str[i])) {
+			++count;
+		} else {
 			break;
 		}
-	}
+	}	
+	int tmp = str.length() - count;
+	str.erase(tmp, count);
 }
 
 int main() {
-	std::string str = "hello        ";
+	std::string str = "hello                    bajd                                         ";
 	rtrim(str);
-	std::cout << str << "a";
+	std::cout << str << "a" << std::endl;
 	return 0;
 }
